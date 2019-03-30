d59a23ccb7fe6ae96cef8eda253b1c6f9c5ba0d5
AthulB <athul1998balakrishnan@gmail.com>
2019-03-30 19:09:37 +0530

    changes made to user contract

--- contracts/UserInfo.sol
+++ contracts/UserInfo.sol
@@ -21,7 +21,7 @@ contract UserInfo {
 
 	//storing number of users
 	uint public userCount;
-	address admin;
+	address public admin;
 	bytes32 admin_license;
 	
 	//Store the users in the system
@@ -38,14 +38,32 @@ contract UserInfo {
 		require (msg.sender == admin); 
 		_; 
 	}
-	
-	function users(bytes32 name, bytes32 email, bytes32 contactNumber) public {
-        // Admin is the first user of the system at index 1
-        userslist[++userCount] = User(admin, name, email, contactNumber, admin_license, userType.Admin);
-        userMap[admin] = userCount;
-    }
 
-	function addUser(address account,bytes32 name,bytes32 email,bytes32 contactNumber, bytes32 licenseid, userType utype) public onlyAdmin{
+	function stringToBytes32(string memory source) public returns (bytes32 result) {
+	    bytes memory tempEmptyStringTest = bytes(source);
+	    if (tempEmptyStringTest.length == 0) {
+	        return 0x0;
+	    }
+
+	    assembly {
+	        result := mload(add(source, 32))
+	    }
+	}
+	
+	// function users(string memory uname, string memory uemail, string memory ucontactNumber) public {
+ //        // Admin is the first user of the system at index 1
+ //        bytes32 name = stringToBytes32(uname);
+ //        bytes32 email = stringToBytes32(uemail);
+ //        bytes32 contactNumber = stringToBytes32(ucontactNumber);
+ //        userslist[++userCount] = User(admin, name, email, contactNumber, admin_license, userType.Admin);
+ //        userMap[admin] = userCount;
+ //    }
+
+	function addUser(address account,string memory uname,string memory uemail,string memory ucontactNumber, string memory ulicenseid, userType utype) public onlyAdmin{
+        bytes32 name = stringToBytes32(uname);
+        bytes32 email = stringToBytes32(uemail);
+        bytes32 contactNumber = stringToBytes32(ucontactNumber);
+        bytes32 licenseid = stringToBytes32(ulicenseid);
         userslist[++userCount] = User(account, name, email, contactNumber, licenseid, utype);
         userMap[account] = userCount;
 	}
